9c391b6ac72ceb98fda63ddeb724141b922f748e
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-02-11 14:45:44 +0200

    Create Frontend Project

---
+++ EmpiriaBMS.Front/Config.cs
@@ -0,0 +1,13 @@
+using Microsoft.TeamsFx.Configuration;
+
+namespace EmpiriaBMS.Front
+{
+    public class ConfigOptions
+    {
+        public TeamsFxOptions TeamsFx { get; set; }
+    }
+    public class TeamsFxOptions
+    {
+        public AuthenticationOptions Authentication { get; set; }
+    }
+}
