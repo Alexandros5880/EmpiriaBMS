59c17869964c1f6c98057f2ec5ec8eb7f5a436c8
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-02-15 19:41:06 +0200

    Hide Peginator If Page Is 1

--- EmpiriaBMS.EF.CLI/obj/Debug/net6.0/EmpiriaBMS.EF.CLI.AssemblyInfo.cs
+++ EmpiriaBMS.EF.CLI/obj/Debug/net6.0/EmpiriaBMS.EF.CLI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpiriaBMS.EF.CLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aced77707a141540ff8b95f7e3c8351c9676252")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+334fa19af144b9b01c8ad50908788697dbf4a5d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpiriaBMS.EF.CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpiriaBMS.EF.CLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
