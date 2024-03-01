3ba084ed200479c35ab976cbeb507828334c0171
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-03-01 18:16:05 +0200

    Add UserTime Compoment Not Get Real Data Yet

--- EmpiriaBMS.Core/obj/Debug/net6.0/EmpiriaBMS.Core.AssemblyInfo.cs
+++ EmpiriaBMS.Core/obj/Debug/net6.0/EmpiriaBMS.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+520bfa38a36c5ff419e0f7735ab4669910b1e0fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f2269533496cd60c82fb86c73e18372ce78769d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
