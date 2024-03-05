2eafd82c351aaa94fe2069bcd6317cd9c1b82cd6
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-03-05 16:47:17 +0200

    No Render Right

--- EmpiriaBMS.Front/Components/Admin/Roles/Roles.razor.cs
+++ EmpiriaBMS.Front/Components/Admin/Roles/Roles.razor.cs
@@ -65,7 +65,6 @@ public partial class Roles
 
     protected override bool ShouldRender()
     {
-        // Return false to prevent the entire component from re-rendering
         return true;
     }
 
