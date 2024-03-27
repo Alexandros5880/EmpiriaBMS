1d6e20dbc7925ec9c42403a115f9b98f633a7199
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-03-27 14:51:25 +0200

    Solution Date -> Unvisible and when i click IsClose then Set And Display DolutionDate.

--- EmpiriaBMS.Core/obj/Debug/net6.0/EmpiriaBMS.Core.AssemblyInfo.cs
+++ EmpiriaBMS.Core/obj/Debug/net6.0/EmpiriaBMS.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9afbb508eb4a09ec6c72bad70ee1dd571303c500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0a10692021a010da72db3f568a9decd47fe51c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ EmpiriaBMS.Front/Components/Issue.razor.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Components;
+using EmpiriaBMS.Core.Config;
+using EmpiriaBMS.Core.Dtos;
+using System.Text.Json;
+using EmpiriaBMS.Front.ViewModel.Components;
+using EmpiriaBMS.Front.Components.General;
+using System.ComponentModel;
+
+namespace EmpiriaBMS.Front.Components;
+
+public partial class Issue : ComponentBase, IDisposable
+{
+    private bool disposedValue;
+    private ProjectVM _project;
+    [Parameter]
+    public ProjectVM Project
+    {
+        get => _project;
+        set
+        {
+            if (value != null)
+            {
+                _project = value;
+                UserDto u = Mapping.Mapper.Map<UserDto>(_project.Customer);
+                _customer = Mapper.Map<UserVM>(u);
+            }
+        }
+    }
+
+    private UserVM _customer;
+
+    private IssueVM _issue = new IssueVM()
+    {
+        ComplaintDate = DateTime.Now,
+        SolutionDate = DateTime.Now,
+        VerificationDate = DateTime.Now
+    };
+
+    SignaturePad verificatorSignature;
+    SignaturePad pMSignature;
+
+    public void Refresh()
+    {
+        _issue = new IssueVM()
+        {
+            ComplaintDate = DateTime.Now,
+            SolutionDate = DateTime.Now,
+            VerificationDate = DateTime.Now
+        };
+        _issue.PropertyChanged += _issuePropertyChanged;
+        StateHasChanged();
+    }
+
+    private void _issuePropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        switch (e.PropertyName)
+        {
+            case nameof(_issue.IsClose):
+                bool isClosed = (sender as IssueVM).IsClose;
+                _issue.SolutionDate = isClosed ? DateTime.Now : _issue.SolutionDate;
+                break;
+        }
+    }
+
+    public async Task HandleValidSubmit()
+    {
+        _issue.VerificatorSignature = await verificatorSignature.GetImageData();
+        _issue.PMSignature = await pMSignature.GetImageData();
+        _issue.ProjectId = _project.Id;
+        _issue.CustomerId = _customer.Id;
+        _issue.ProjectManagerId = _project.ProjectManager.Id;
+
+        // TODO: Validate And Save Complain
+        if (_issue.ComplaintDate <= DateTime.Now.AddDays(-1) || _issue.ComplaintDate == null)
+        {
+
+        }
+        if (string.IsNullOrEmpty(_issue.About))
+        {
+
+        }
+        if (string.IsNullOrEmpty(_issue.Description))
+        {
+
+        }
+        if (string.IsNullOrEmpty(_issue.Solution))
+        {
+
+        }
+        if (_issue.SolutionDate <= DateTime.Now.AddDays(-1) || _issue.SolutionDate == null)
+        {
+
+        }
+        if (string.IsNullOrEmpty(_issue.Evaluation))
+        {
+
+        }
+        if (string.IsNullOrEmpty(_issue.Verification))
+        {
+
+        }
+        if (_issue.VerificationDate <= DateTime.Now.AddDays(-1) || _issue.VerificationDate == null)
+        {
+
+        }
+        if (_issue.VerificatorSignature.Length == 0)
+        {
+
+        }
+        if (_issue.PMSignature.Length == 0)
+        {
+
+        }
+
+        await DataProvider.Issues.Add(Mapper.Map<IssueDto>(_issue));
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                _issue.PropertyChanged -= _issuePropertyChanged;
+            }
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
