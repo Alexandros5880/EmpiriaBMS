3fd603900dd25b1ce643f933d2a5e3355f25baec
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-03-29 18:30:24 +0200

    Save.

--- EmpiriaBMS.Core/obj/Debug/net6.0/EmpiriaBMS.Core.AssemblyInfo.cs
+++ EmpiriaBMS.Core/obj/Debug/net6.0/EmpiriaBMS.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+411001c6bb0a228dbc39f68e24e9d1171e639a6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17fc10d8916433d6812bdf333914f099d89fb9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
