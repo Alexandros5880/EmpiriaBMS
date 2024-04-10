92fee7310658187f2c739a65dd857b04fe43d2cf
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-04-10 14:30:43 +0300

    Add Google PLave Id API Service.

---
+++ EmpiriaBMS.Core/Services/GooglePlaces/Models/GooglePlaceDetailsResponse.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpiriaBMS.Core.Services.GooglePlaces.Models;
+
+public class GooglePlaceDetailsResponse
+{
+    [JsonProperty("candidates")]
+    public List<GooglePlaceCandidate> Candidates { get; set; }
+}
--- EmpiriaBMS.Models/Models/Address.cs
+++ EmpiriaBMS.Models/Models/Address.cs
@@ -10,12 +10,15 @@ namespace EmpiriaBMS.Models.Models;
 
 public class Address : Entity
 {
+    public string FormattedAddress { get; set; }
     public string Street { get; set; }
     public string City { get; set; }
     public string State { get; set; }
     public string PostalCode { get; set; }
     public string Country { get; set; }
-    public string GooglePlaceId { get; set; }
+    public string PlaceId { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
 
     public List<Project> Projects { get; set; }
 }
