f346c1eec00478947bfb1da0169ac5eb2b85c9f7
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-04-16 20:24:18 +0300

    Add Offers Table OK.

--- EmpiriaBMS.Core/Repositories/OfferRepo.cs
+++ EmpiriaBMS.Core/Repositories/OfferRepo.cs
@@ -1,10 +1,13 @@
-﻿using EmpiriaBMS.Core.Dtos;
+﻿using EmpiriaBMS.Core.Config;
+using EmpiriaBMS.Core.Dtos;
 using EmpiriaBMS.Core.Repositories.Base;
 using EmpiriaBMS.Models.Models;
+using EmpiriaMS.Models.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,4 +16,18 @@ namespace EmpiriaBMS.Core.Repositories;
 public class OfferRepo : Repository<OfferDto, Offer>
 {
     public OfferRepo(IDbContextFactory<AppDbContext> DbFactory) : base(DbFactory) { }
+
+    public async Task<ICollection<OfferDto>> GetAll(int stateId = 0, int typeId = 0)
+    {
+        using (var _context = _dbContextFactory.CreateDbContext())
+        {
+            var offers = await _context.Set<Offer>()
+                                         .Where(o => stateId == 0 ? true : o.StateId == stateId && typeId == 0 ? true : o.TypeId == typeId)
+                                         .Include(o => o.State)
+                                         .Include(o => o.Type)
+                                         .ToListAsync();
+
+            return Mapping.Mapper.Map<List<Offer>, List<OfferDto>>(offers);
+        }
+    }
 }
---
+++ EmpiriaBMS.Front/Components/Offers.razor.cs
@@ -0,0 +1,91 @@
+﻿using EmpiriaBMS.Front.Interop.TeamsSDK;
+using EmpiriaBMS.Front.Services;
+using EmpiriaBMS.Front.ViewModel.Components;
+using EmpiriaBMS.Models.Models;
+using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+
+namespace EmpiriaBMS.Front.Components;
+
+public partial class Offers
+{
+    private ObservableCollection<OfferVM> _offers = new ObservableCollection<OfferVM>();
+    private ObservableCollection<OfferStateVM> _offerStates = new ObservableCollection<OfferStateVM>();
+    private ObservableCollection<OfferTypeVM> _offerTypes = new ObservableCollection<OfferTypeVM>();
+
+    private OfferStateVM _selectedOfferState;
+    public OfferStateVM SelectedOfferState
+    {
+        get => _selectedOfferState;
+        set
+        {
+            if (_selectedOfferState != value)
+            {
+                _selectedOfferState = value;
+                _getOffers(refresh: true);
+            }
+        }
+    }
+
+    private OfferTypeVM _selectedOfferType;
+    public OfferTypeVM SelectedOfferType
+    {
+        get => _selectedOfferType;
+        set
+        {
+            if (_selectedOfferType != value)
+            {
+                _selectedOfferType = value;
+                _getOffers(refresh: true);
+            }
+        }
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            var runInTeams = await MicrosoftTeams.IsInTeams();
+
+            await Refresh();
+        }
+    }
+
+    private async Task _getOfferStates()
+    {
+        var dtos = await _dataProvider.OfferStates.GetAll();
+        var vms = Mapper.Map<List<OfferStateVM>>(dtos);
+        _offerStates.Clear();
+        vms.ForEach(_offerStates.Add);
+    }
+
+    private async Task _getOfferTypes()
+    {
+        var dtos = await _dataProvider.OfferTypes.GetAll();
+        var vms = Mapper.Map<List<OfferTypeVM>>(dtos);
+        _offerTypes.Clear();
+        vms.ForEach(_offerTypes.Add);
+    }
+
+    private async Task _getOffers(int stateId = 0, int typeId = 0, bool refresh = false)
+    {
+        var dtos = await _dataProvider.Offers.GetAll(stateId, typeId);
+        var vms = Mapper.Map<List<OfferVM>>(dtos);
+        _offers.Clear();
+        vms.ForEach(_offers.Add);
+        if (refresh)
+            StateHasChanged();
+    }
+
+    private async Task Refresh()
+    {
+        await _getOfferStates();
+        await _getOfferTypes();
+        _selectedOfferState = _offerStates.FirstOrDefault();
+        _selectedOfferType = _offerTypes.FirstOrDefault();
+        await _getOffers(_selectedOfferState.Id, _selectedOfferType.Id);
+        StateHasChanged();  
+    }
+}
