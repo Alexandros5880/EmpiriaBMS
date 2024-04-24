998478aa3658bafd22ec3eebede16bd214de89c1
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-04-24 17:20:02 +0300

    Admin Roles Table OK.

--- EmpiriaBMS.Front/Components/Admin/Roles/Roles.razor.cs
+++ EmpiriaBMS.Front/Components/Admin/Roles/Roles.razor.cs
@@ -2,6 +2,7 @@
 using EmpiriaBMS.Core;
 using EmpiriaBMS.Front.Components.General;
 using EmpiriaBMS.Front.ViewModel.Components;
+using Microsoft.AspNetCore.Components;
 using Microsoft.Bot.Builder;
 using Microsoft.Fast.Components.FluentUI;
 using System.Collections.ObjectModel;
@@ -10,21 +11,52 @@ namespace EmpiriaBMS.Front.Components.Admin.Roles;
 
 public partial class Roles
 {
-    private Paginator _paginator;
-    private ObservableCollection<RoleVM> _roles = new ObservableCollection<RoleVM>();
-    private RoleVM _selectedRole = new RoleVM();
+    #region Data Grid
+    private List<RoleVM> _records = new List<RoleVM>();
+    private string _filterString = string.Empty;
+    IQueryable<RoleVM>? FilteredItems => _records?.AsQueryable().Where(x => x.Name.Contains(_filterString, StringComparison.CurrentCultureIgnoreCase));
+    PaginationState pagination = new PaginationState { ItemsPerPage = 10 };
 
-    private EditRole _editRoleCompoment;
+    private RoleVM _selectedRecord = new RoleVM();
 
-    // Add / Edit Dialog
-    private bool _isEditMode = false;
-    //private FluentDialog? _addEditDialog;
-    //private bool _isAddEditDialogOdepened = false;
-
-    protected override void OnInitialized()
+    private void HandleFilter(ChangeEventArgs args)
     {
-        base.OnInitialized();
+        if (args.Value is string value)
+        {
+            _filterString = value;
+        }
+        else if (string.IsNullOrWhiteSpace(_filterString) || string.IsNullOrEmpty(_filterString))
+        {
+            _filterString = string.Empty;
+        }
     }
+
+    private void HandleRowFocus(FluentDataGridRow<RoleVM> row)
+    {
+        _selectedRecord = row.Item as RoleVM;
+    }
+
+    private async Task _getRecords()
+    {
+        var dtos = await DataProvider.Roles.GetAll();
+        _records = Mapper.Map<List<RoleVM>>(dtos);
+    }
+
+    private void _add()
+    {
+
+    }
+
+    private void _edit(RoleVM record)
+    {
+
+    }
+
+    private void _delete(RoleVM record)
+    {
+
+    }
+    #endregion
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
@@ -32,63 +64,9 @@ public partial class Roles
 
         if (firstRender)
         {
-            _paginator.SetRecordsLength(await DataProvider.Roles.Count());
-            await _getRoles();
+            await _getRecords();
 
             StateHasChanged();
         }
-    }
-
-    private async Task _getRoles()
-    {
-        var dtos = await DataProvider.Roles.GetAll(_paginator.PageSize, _paginator.PageIndex);
-        var vms = Mapper.Map<List<RoleVM>>(dtos);
-        _roles.Clear();
-        vms.ForEach(_roles.Add);
-    }
-
-    private void _onSelectRole(int id)
-    {
-        _selectedRole = _roles.FirstOrDefault(r => r.Id == id);
-    }
-
-    protected override bool ShouldRender()
-    {
-        return true;
-    }
-
-    private async Task _add()
-    {
-        _selectedRole = null;
-        _isEditMode = true;
-        //_addEditDialog.Show();
-        //_isAddEditDialogOdepened = true;
-    }
-
-    private async Task _edit()
-    {
-        _isEditMode = true;
-        //_addEditDialog.Show();
-        //_isAddEditDialogOdepened = true;
-    }
-
-    private async Task _delete(int id)
-    {
-        await Task.Delay(1333);
-    }
-
-    private async Task _save()
-    {
-        await Task.Delay(1333);
-        _isEditMode = false;
-        //_addEditDialog.Hide();
-        //_isAddEditDialogOdepened = false;
-    }
-
-    private void _cancel()
-    {
-        _isEditMode = false;
-        //_addEditDialog.Hide();
-        //_isAddEditDialogOdepened = false;
     }
 }
