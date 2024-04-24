4a5d48a7123c54f8531b66c5e71ce4c6d2177d85
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-04-24 19:30:10 +0300

    Admin Add Clients Table.

---
+++ EmpiriaBMS.Front/Components/Admin/Projects/Clients/Clients.razor.cs
@@ -0,0 +1,67 @@
+﻿using EmpiriaBMS.Front.ViewModel.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Fast.Components.FluentUI;
+
+namespace EmpiriaBMS.Front.Components.Admin.Projects.Clients;
+
+public partial class Clients
+{
+    #region Data Grid
+    private List<ClientVM> _records = new List<ClientVM>();
+    private string _filterString = string.Empty;
+    IQueryable<ClientVM>? FilteredItems => _records?.AsQueryable().Where(x => x.FullName.Contains(_filterString, StringComparison.CurrentCultureIgnoreCase));
+    PaginationState pagination = new PaginationState { ItemsPerPage = 10 };
+
+    private ClientVM _selectedRecord = new ClientVM();
+
+    private void HandleFilter(ChangeEventArgs args)
+    {
+        if (args.Value is string value)
+        {
+            _filterString = value;
+        }
+        else if (string.IsNullOrWhiteSpace(_filterString) || string.IsNullOrEmpty(_filterString))
+        {
+            _filterString = string.Empty;
+        }
+    }
+
+    private void HandleRowFocus(FluentDataGridRow<ClientVM> row)
+    {
+        _selectedRecord = row.Item as ClientVM;
+    }
+
+    private async Task _getRecords()
+    {
+        var dtos = await DataProvider.Clients.GetAll();
+        _records = Mapper.Map<List<ClientVM>>(dtos);
+    }
+
+    private void _add()
+    {
+
+    }
+
+    private void _edit(ClientVM record)
+    {
+
+    }
+
+    private void _delete(ClientVM record)
+    {
+
+    }
+    #endregion
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            await _getRecords();
+
+            StateHasChanged();
+        }
+    }
+}
--- EmpiriaBMS.Front/ViewModel/Components/ClientVM.cs
+++ EmpiriaBMS.Front/ViewModel/Components/ClientVM.cs
@@ -46,4 +46,5 @@ public class ClientVM : UserVM
     }
 
     public ICollection<Project> Projects { get; set; }
+
 }
