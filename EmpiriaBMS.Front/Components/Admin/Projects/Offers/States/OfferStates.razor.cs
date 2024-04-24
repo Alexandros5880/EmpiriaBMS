d40097c8ff75154ba465ca6807666ed8b3f2bfb2
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-04-24 20:10:16 +0300

    Admin Add Offers Types/States Table.

---
+++ EmpiriaBMS.Front/Components/Admin/Projects/Offers/States/OfferStates.razor.cs
@@ -0,0 +1,67 @@
+﻿using EmpiriaBMS.Front.ViewModel.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Fast.Components.FluentUI;
+
+namespace EmpiriaBMS.Front.Components.Admin.Projects.Offers.States;
+
+public partial class OfferStates
+{
+    #region Data Grid
+    private List<OfferStateVM> _records = new List<OfferStateVM>();
+    private string _filterString = string.Empty;
+    IQueryable<OfferStateVM>? FilteredItems => _records?.AsQueryable().Where(x => x.Name.Contains(_filterString, StringComparison.CurrentCultureIgnoreCase));
+    PaginationState pagination = new PaginationState { ItemsPerPage = 10 };
+
+    private OfferStateVM _selectedRecord = new OfferStateVM();
+
+    private void HandleFilter(ChangeEventArgs args)
+    {
+        if (args.Value is string value)
+        {
+            _filterString = value;
+        }
+        else if (string.IsNullOrWhiteSpace(_filterString) || string.IsNullOrEmpty(_filterString))
+        {
+            _filterString = string.Empty;
+        }
+    }
+
+    private void HandleRowFocus(FluentDataGridRow<OfferStateVM> row)
+    {
+        _selectedRecord = row.Item as OfferStateVM;
+    }
+
+    private async Task _getRecords()
+    {
+        var dtos = await DataProvider.OfferStates.GetAll();
+        _records = Mapper.Map<List<OfferStateVM>>(dtos);
+    }
+
+    private void _add()
+    {
+
+    }
+
+    private void _edit(OfferStateVM record)
+    {
+
+    }
+
+    private void _delete(OfferStateVM record)
+    {
+
+    }
+    #endregion
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            await _getRecords();
+
+            StateHasChanged();
+        }
+    }
+}
---
+++ EmpiriaBMS.Front/Components/Admin/Projects/Offers/Types/OfferTypes.razor.cs
@@ -0,0 +1,67 @@
+﻿using EmpiriaBMS.Front.ViewModel.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Fast.Components.FluentUI;
+
+namespace EmpiriaBMS.Front.Components.Admin.Projects.Offers.Types;
+
+public partial class OfferTypes
+{
+    #region Data Grid
+    private List<OfferTypeVM> _records = new List<OfferTypeVM>();
+    private string _filterString = string.Empty;
+    IQueryable<OfferTypeVM>? FilteredItems => _records?.AsQueryable().Where(x => x.Name.Contains(_filterString, StringComparison.CurrentCultureIgnoreCase));
+    PaginationState pagination = new PaginationState { ItemsPerPage = 10 };
+
+    private OfferTypeVM _selectedRecord = new OfferTypeVM();
+
+    private void HandleFilter(ChangeEventArgs args)
+    {
+        if (args.Value is string value)
+        {
+            _filterString = value;
+        }
+        else if (string.IsNullOrWhiteSpace(_filterString) || string.IsNullOrEmpty(_filterString))
+        {
+            _filterString = string.Empty;
+        }
+    }
+
+    private void HandleRowFocus(FluentDataGridRow<OfferTypeVM> row)
+    {
+        _selectedRecord = row.Item as OfferTypeVM;
+    }
+
+    private async Task _getRecords()
+    {
+        var dtos = await DataProvider.OfferTypes.GetAll();
+        _records = Mapper.Map<List<OfferTypeVM>>(dtos);
+    }
+
+    private void _add()
+    {
+
+    }
+
+    private void _edit(OfferTypeVM record)
+    {
+
+    }
+
+    private void _delete(OfferTypeVM record)
+    {
+
+    }
+    #endregion
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            await _getRecords();
+
+            StateHasChanged();
+        }
+    }
+}
