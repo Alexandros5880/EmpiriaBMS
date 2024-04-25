a4aaf37bb43409f310954a9d62485ac0b302f0e1
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-04-25 20:50:12 +0300

    Add Edit SupportiveWork Type OK.

--- EmpiriaBMS.Front/Components/Admin/SupportiveWorks/Types/SupportiveWorkTypes.razor.cs
+++ EmpiriaBMS.Front/Components/Admin/SupportiveWorks/Types/SupportiveWorkTypes.razor.cs
@@ -1,4 +1,6 @@
-﻿using EmpiriaBMS.Front.ViewModel.Components;
+﻿using EmpiriaBMS.Core.Dtos;
+using EmpiriaBMS.Front.Components.Admin.General;
+using EmpiriaBMS.Front.ViewModel.Components;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Fast.Components.FluentUI;
 
@@ -37,14 +39,56 @@ public partial class SupportiveWorkTypes
         _records = Mapper.Map<List<OtherTypeVM>>(dtos);
     }
 
-    private void _add()
+    private async Task _add()
     {
+        DialogParameters parameters = new()
+        {
+            Title = $"New Record",
+            PrimaryActionEnabled = true,
+            SecondaryActionEnabled = true,
+            PrimaryAction = "Save",
+            SecondaryAction = "Cancel",
+            TrapFocus = true,
+            Modal = true,
+            PreventScroll = true
+        };
 
+        IDialogReference dialog = await DialogService.ShowDialogAsync<UniqueTypeForm>(new OtherTypeVM(), parameters);
+        DialogResult? result = await dialog.Result;
+
+        if (result.Data is not null)
+        {
+            OtherTypeVM vm = result.Data as OtherTypeVM;
+            var dto = Mapper.Map<OtherTypeDto>(vm);
+            await DataProvider.OthersTypes.Add(dto);
+            await _getRecords();
+        }
     }
 
-    private void _edit(OtherTypeVM record)
+    private async Task _edit(OtherTypeVM record)
     {
+        DialogParameters parameters = new()
+        {
+            Title = $"Edit {record.Name}",
+            PrimaryActionEnabled = true,
+            SecondaryActionEnabled = true,
+            PrimaryAction = "Save",
+            SecondaryAction = "Cancel",
+            TrapFocus = true,
+            Modal = true,
+            PreventScroll = true
+        };
 
+        IDialogReference dialog = await DialogService.ShowDialogAsync<UniqueTypeForm>(record, parameters);
+        DialogResult? result = await dialog.Result;
+
+        if (result.Data is not null)
+        {
+            OtherTypeVM vm = result.Data as OtherTypeVM;
+            var dto = Mapper.Map<OtherTypeDto>(vm);
+            await DataProvider.OthersTypes.Update(dto);
+            await _getRecords();
+        }
     }
 
     private async Task _delete(OtherTypeVM record)
