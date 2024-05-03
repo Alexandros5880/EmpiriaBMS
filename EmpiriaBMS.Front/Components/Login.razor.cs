cd85aa32e33e460f987ae8b2cacc39d8dd46efeb
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-05-03 22:47:23 +0300

    Add Login Page.

---
+++ EmpiriaBMS.Front/Components/Login.razor.cs
@@ -0,0 +1,30 @@
+﻿using EmpiriaBMS.Front.Interop.TeamsSDK;
+using Microsoft.AspNetCore.Components;
+
+namespace EmpiriaBMS.Front.Components;
+
+public partial class Login
+{
+    private string username;
+    private string password;
+    private string errorMessage;
+
+    private void HandleSubmit()
+    {
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+        {
+            errorMessage = "Please enter both username and password.";
+            return;
+        }
+
+        // TODO: Login Check For User to Server. If User Not Exists Notify CEO, CTO, COO, Admin
+        if (username == "admin" && password == "password")
+        {
+            MyNavigationManager.NavigateTo("/dashboard");
+        }
+        else
+        {
+            errorMessage = "Invalid username or password.";
+        }
+    }
+}
