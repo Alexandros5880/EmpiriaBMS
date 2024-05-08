60ec01ece18e5c851bbb67bc12ef3d0f6ca32ac3
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-05-08 19:09:55 +0300

    Save

--- EmpiriaBMS.Front/Components/Login.razor.cs
+++ EmpiriaBMS.Front/Components/Login.razor.cs
@@ -1,5 +1,15 @@
-﻿using EmpiriaBMS.Front.Interop.TeamsSDK;
+﻿using AutoMapper;
+using EmpiriaBMS.Core.Dtos;
+using EmpiriaBMS.Core.Hellpers;
+using EmpiriaBMS.Front.ViewModel.Components;
+using EmpiriaBMS.Front.ViewModel.Interfaces;
+using EmpiriaBMS.Models.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Fast.Components.FluentUI;
+using Microsoft.Graph.Models;
+using System;
+using System.Text.RegularExpressions;
+using static Microsoft.Fast.Components.FluentUI.Emojis.Objects.Color.Default;
 
 namespace EmpiriaBMS.Front.Components;
 
@@ -11,7 +21,7 @@ public partial class Login
 
     private void HandleSubmit()
     {
-        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || !Validate())
         {
             errorMessage = "Please enter both username and password.";
             return;
@@ -27,4 +37,40 @@ public partial class Login
             errorMessage = "Invalid username or password.";
         }
     }
+
+    #region Validation
+    private bool validUserName = true;
+    private bool validPassword = true;
+
+    private bool Validate(string fieldname = null)
+    {
+        if (fieldname == null)
+        {
+            validUserName = _isvalidUserName(username);
+            validPassword = !string.IsNullOrEmpty(password);
+
+            return validUserName && validPassword;
+        }
+        else
+        {
+            validUserName = true;
+            validPassword = true;
+
+            switch (fieldname)
+            {
+                case "Emails":
+                    validUserName = _isvalidUserName(username);
+                    return validUserName;
+                case "Password":
+                    validPassword = !string.IsNullOrEmpty(password);
+                    return validPassword;
+                default:
+                    return true;
+            }
+
+        }
+    }
+
+    private bool _isvalideMAIL(string email) => GeneralValidator.IsValidEmail(email);
+    #endregion
 }
