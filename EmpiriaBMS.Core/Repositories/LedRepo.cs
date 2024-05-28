8cb3fcc17894a6e6d30f86bd4bbd41eb1d14a0b5
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-05-28 19:36:28 +0300

    Admin Leds Table OK.

--- EmpiriaBMS.Core/Repositories/LedRepo.cs
+++ EmpiriaBMS.Core/Repositories/LedRepo.cs
@@ -15,6 +15,25 @@ public class LedRepo : Repository<LedDto, Led>, IDisposable
 {
     public LedRepo(IDbContextFactory<AppDbContext> DbFactory) : base(DbFactory) { }
 
+    public async Task<LedDto?> Get(int id)
+    {
+        if (id == 0)
+            throw new ArgumentNullException(nameof(id));
+
+        using (var _context = _dbContextFactory.CreateDbContext())
+        {
+            var i = await _context
+                             .Set<Led>()
+                             .Where(r => !r.IsDeleted)
+                             .Include(l => l.Address)
+                             .Include(l => l.Client)
+                             .Include(l => l.Offer)
+                             .FirstOrDefaultAsync(r => r.Id == id);
+
+            return Mapping.Mapper.Map<LedDto>(i);
+        }
+    }
+
     public new async Task<ICollection<LedDto>> GetAll(int pageSize = 0, int pageIndex = 0)
     {
         using (var _context = _dbContextFactory.CreateDbContext())
