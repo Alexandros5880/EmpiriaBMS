40fc9f052780498a4ab187704e0b488f868ad5d9
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-06-04 13:59:44 +0300

    Save

--- EmpiriaBMS.Front/Components/Admin/Leds/LedDetailed.razor.cs
+++ EmpiriaBMS.Front/Components/Admin/Leds/LedDetailed.razor.cs
@@ -31,6 +31,8 @@ public partial class LedDetailed
 
     private bool _displayClientForm = false;
 
+    private bool _isMapVisible { get; set; } = false;
+
     private void _toogleClientForm(bool? display = null)
     {
         if (display == null)
