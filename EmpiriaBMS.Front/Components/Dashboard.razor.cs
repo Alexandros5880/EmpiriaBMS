5e3604ba56b9afdd003f8228f91e095690651457
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-06-07 19:15:41 +0300

    Save.

--- EmpiriaBMS.Front/Components/Dashboard.razor.cs
+++ EmpiriaBMS.Front/Components/Dashboard.razor.cs
@@ -316,7 +316,7 @@ public partial class Dashboard : IDisposable
         try
         {
             // Get Projects of last month
-            List<ProjectDto> projectsDto = (await _dataProvider.Projects.GetLastMonthProjects(_sharedAuthData.LogedUser.Id, offerId: _selectedOffer?.Id ?? 0), active).ToList<ProjectDto>();
+            List<ProjectDto> projectsDto = (await _dataProvider.Projects.GetLastMonthProjects(_sharedAuthData.LogedUser.Id, offerId: _selectedOffer?.Id ?? 0, active: active)).ToList<ProjectDto>();
 
 
             var projectsVm = Mapper.Map<List<ProjectDto>, List<ProjectVM>>(projectsDto);
