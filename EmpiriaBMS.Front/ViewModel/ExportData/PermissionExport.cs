28dd32efda9e5244b8ce469a9e85930667eb7bad
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-06-11 19:57:38 +0300

    Export Permissions OK.

---
+++ EmpiriaBMS.Front/ViewModel/ExportData/PermissionExport.cs
@@ -0,0 +1,16 @@
+﻿using EmpiriaBMS.Front.ViewModel.Components;
+
+namespace EmpiriaBMS.Front.ViewModel.ExportData;
+
+public class PermissionExport
+{
+    public string Name { get; set; }
+
+    public int Ord { get; set; }
+
+    public PermissionExport(PermissionVM model)
+    {
+        Name = model.Name;
+        Ord = model.Ord;
+    }
+}
