5e8767e605a1cd18d08f7621ceb46ba45697e08f
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-06-11 17:08:22 +0300

    Export Deliverables OK.

---
+++ EmpiriaBMS.Front/ViewModel/ExportData/DeliverableExport.cs
@@ -0,0 +1,34 @@
+﻿using EmpiriaBMS.Front.ViewModel.Components;
+
+namespace EmpiriaBMS.Front.ViewModel.ExportData;
+
+public class DeliverableExport
+{
+    public int TypeId { get; set; }
+
+    public string TypeName { get; set; }
+
+    public int DisciplineId { get; set; }
+
+    public string DisciplineType { get; set; }
+
+    public int ProjectId { get; set; }
+
+    public string ProjectName { get; set; }
+
+    public float CompletionEstimation { get; set; }
+
+    public string CompletionDate { get; set; }
+
+    public DeliverableExport(DrawingVM model)
+    {
+        TypeId = model.TypeId ?? 0;
+        TypeName = model.Type?.Name ?? "";
+        DisciplineId = model.DisciplineId ?? 0;
+        DisciplineType = model.Discipline?.Type?.Name ?? "";
+        ProjectId = model.Discipline?.ProjectId ?? 0;
+        ProjectName = model.Discipline?.Project?.Name ?? "";
+        CompletionEstimation = model.CompletionEstimation;
+        CompletionDate = model.CompletionDate?.ToEuropeFormat() ?? "";
+    }
+}
