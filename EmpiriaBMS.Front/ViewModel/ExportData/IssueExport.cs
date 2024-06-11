960b561e42975ca3fbe7301eafe1b74cce71cf05
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-06-11 20:41:06 +0300

    Export Issues OK.

---
+++ EmpiriaBMS.Front/ViewModel/ExportData/IssueExport.cs
@@ -0,0 +1,55 @@
+﻿using EmpiriaBMS.Front.ViewModel.Components;
+
+namespace EmpiriaBMS.Front.ViewModel.ExportData;
+
+public class IssueExport
+{
+    public string ComplaintDate { get; set; }
+
+    public int ProjectId { get; set; }
+
+    public string ProjectName { get; set; }
+
+    public string ProjectCode { get; set; }
+
+    public int DisplayedRoleId { get; set; }
+
+    public string DisplayedRole { get; set; }
+
+    public int CreatorId { get; set; }
+
+    public string Creator { get; set; }
+
+    public string Description { get; set; }
+
+    public string Solution { get; set; }
+
+    public string SolutionDate { get; set; }
+
+    public string Evaluation { get; set; }
+
+    public string Verification { get; set; }
+
+    public string VerificationDate { get; set; }
+
+    public bool IsClose { get; set; }
+
+    public IssueExport(IssueVM model)
+    {
+        ComplaintDate = model.ComplaintDate?.ToEuropeFormat() ?? "";
+        ProjectId = model.ProjectId;
+        ProjectName = model.Project?.Name ?? "";
+        ProjectCode = model.Project?.Code ?? "";
+        DisplayedRoleId = model.DisplayedRoleId;
+        DisplayedRole = model.DisplayedRole?.Name ?? "";
+        CreatorId = model.CreatorId;
+        Creator = model.Creator?.FullName ?? "";
+        Description = model.Description ?? "";
+        Solution = model.Solution ?? "";
+        SolutionDate = model.SolutionDate?.ToEuropeFormat() ?? "";
+        Evaluation = model.Evaluation ?? "";
+        Verification = model.Verification ?? "";
+        VerificationDate = model.VerificationDate?.ToEuropeFormat() ?? "";
+        IsClose = model.IsClose;
+    }
+}
