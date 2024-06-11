77a275cf96fd689b2dcf3eb96304b219fa04039e
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-06-11 17:14:36 +0300

    Export Deliverable-Types OK.

---
+++ EmpiriaBMS.Front/ViewModel/ExportData/DeliverableTypeExport.cs
@@ -0,0 +1,16 @@
+﻿using EmpiriaBMS.Front.ViewModel.Components;
+
+namespace EmpiriaBMS.Front.ViewModel.ExportData;
+
+public class DeliverableTypeExport
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public DeliverableTypeExport(DrawingTypeVM model)
+    {
+        Name = model.Name;
+        Description = model.Description ?? "";
+    }
+}
