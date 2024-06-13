64f9d8f7bad8a9156d8684090db3ea1633871b34
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-06-13 20:59:26 +0300

    Import OfferTypes from csv OK.

--- EmpiriaBMS.Front/Components/Admin/Offers/Types/OfferTypes.razor.cs
+++ EmpiriaBMS.Front/Components/Admin/Offers/Types/OfferTypes.razor.cs
@@ -4,6 +4,7 @@ using EmpiriaBMS.Front.Components.Admin.General;
 using EmpiriaBMS.Front.ViewModel.Components;
 using EmpiriaBMS.Front.ViewModel.ExportData;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.Fast.Components.FluentUI;
 
 namespace EmpiriaBMS.Front.Components.Admin.Offers.Types;
@@ -121,7 +122,7 @@ public partial class OfferTypes
         }
     }
 
-    #region Export Data
+    #region Import/Export Data
     private async Task ExportToCSV()
     {
         var date = DateTime.Today;
@@ -132,6 +133,43 @@ public partial class OfferTypes
             string csvContent = Data.GetCsvContent(data);
             await MicrosoftTeams.DownloadCsvFile(fileName, csvContent);
         }
+    }
+
+    private InputFile fileInput;
+    private async Task ImportFromCSV(InputFileChangeEventArgs e)
+    {
+        var file = e.File;
+        var filePath = file.Name;
+        var fileType = file.ContentType;
+        if (fileType?.Equals("text/csv") ?? false)
+        {
+            try
+            {
+                Stream stream = file.OpenReadStream();
+                List<OfferTypeExport> data = await Data.ImportData<OfferTypeExport>(stream);
+                if (data != null && data.Count > 0)
+                {
+                    foreach (var item in data)
+                    {
+                        var vm = item.Get();
+                        var dto = Mapper.Map<OfferTypeDto>(vm);
+                        var added = await DataProvider.OfferTypes.Add(dto);
+                        var addedDto = Mapper.Map<OfferTypeVM>(added);
+                        _records.Add(addedDto);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Offer Types import: {ex.Message}, \nInner: {ex.InnerException?.Message}");
+                // TODO: log error
+            }
+        }
+    }
+    private async Task TriggerFileInput()
+    {
+        var element = fileInput.Element;
+        await MicrosoftTeams.TriggerFileInputClick(element);
     }
     #endregion
 }
