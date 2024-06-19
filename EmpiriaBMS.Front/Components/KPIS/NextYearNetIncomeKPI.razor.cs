5101f14ed0337b0a3d861d59d94fcd7f64830db8
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-06-19 19:10:45 +0300

    Add SeeNextYearIncome Compoment.

---
+++ EmpiriaBMS.Front/Components/KPIS/NextYearNetIncomeKPI.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace EmpiriaBMS.Front.Components.KPIS;
+
+public partial class NextYearNetIncomeKPI
+{
+
+    private double _nextYearNetIncome = 0;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            _nextYearNetIncome = await _dataProvider.KPIS.GetNextYearNetIncome();
+        }
+    }
+
+}
