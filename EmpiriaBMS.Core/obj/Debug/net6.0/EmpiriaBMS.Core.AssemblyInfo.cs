a8a04a1506472a351e2bea71c7b518ccf184a9f2
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-06-20 20:00:04 +0300

    Upload Download Buttons Add Icons

--- EmpiriaBMS.Core/obj/Debug/net6.0/EmpiriaBMS.Core.AssemblyInfo.cs
+++ EmpiriaBMS.Core/obj/Debug/net6.0/EmpiriaBMS.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+153519f4ebccf17f9218da0fb7c87badb6e85e2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9bc828aedabcfe3bc972f392379166a471b5131")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpiriaBMS.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
