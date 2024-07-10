18f2cc176256e670a764b042d1442b2c2ce0f1b4
Alexandros5880 <alexandrosplatanios1@gmail.com>
2024-07-10 17:16:36 +0300

    Save

---
+++ EmpiriaBMS.Front/Components/WorkingHours/DailyTimeRequestsCorrection.razor.cs
@@ -0,0 +1,13 @@
+﻿using EmpiriaBMS.Models.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EmpiriaBMS.Front.Components.WorkingHours;
+
+public partial class DailyTimeRequestsCorrection
+{
+    [Parameter]
+    public int RequestCount { get; set; }
+
+    [Parameter]
+    public Dictionary<Type, List<DailyTimeRequest>> DailyRequests { get; set; }
+}
